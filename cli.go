aae8c7897202b32bb6f6b1d21fd4a5d3aa22e75a
Steve Katz <steve@teacherspayteachers.com>
2019-01-27 19:29:01 -0500

    Implement create table

--- cli.go
+++ cli.go
@@ -1,14 +1,13 @@
 package main
 
-import "bufio"
-import "fmt"
-
-import "os"
-
-import "strings"
 import (
-	"github.com/davecgh/go-spew/spew"
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	//"github.com/davecgh/go-spew/spew"
 	"github.com/xwb1989/sqlparser"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -16,8 +15,37 @@ func main() {
 	for {
 		fmt.Print("stvql> ")
 		input, _ := reader.ReadString('\n')
-		sql := strings.Trim(input, "\n")
-		stmt, _ := sqlparser.Parse(sql)
-		spew.Dump(stmt)
+		raw := strings.Trim(input, "\n")
+		sql, _ := sqlparser.Parse(raw)
+		switch stmt := sql.(type) {
+		case *sqlparser.DDL:
+			// get the column names then initialize a CSV with them
+			tableName := stmt.NewName.Name
+			columnNames := make([]sqlparser.ColIdent, 0)
+			for _, column := range stmt.TableSpec.Columns {
+				columnNames = append(columnNames, column.Name)
+			}
+			f, _ := os.Create(fmt.Sprintf("./%s", tableName))
+			defer f.Close()
+			csvWriter := csv.NewWriter(f)
+			defer csvWriter.Flush()
+			toWrite := []string{}
+			for _, value := range columnNames {
+				toWrite = append(toWrite, sqlparser.String(value))
+			}
+			err := csvWriter.Write(toWrite)
+			checkError("Cannot write to file", err)
+			csvWriter.Flush()
+		case *sqlparser.Select:
+			fmt.Println("***got a select***")
+		case *sqlparser.Insert:
+			fmt.Println("***got an insert***")
+		}
+	}
+}
+
+func checkError(message string, err error) {
+	if err != nil {
+		fmt.Println("WTF")
 	}
 }
